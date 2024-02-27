b79a74ef1681acbd9dd529741ad0ab9c9f12317c
Lucas Picinin <lucaspicinin@hotmail.com>
2024-02-27 10:07:43 -0300

    Correçao - consulta funcionando

--- Scripts/consulta_100repositorios_github.py
+++ Scripts/consulta_100repositorios_github.py
@@ -1,57 +1,85 @@
 import requests
-import json  # Importe o módulo json
+import json
 
-# Substitua 'YOUR_ACCESS_TOKEN' pelo seu token de acesso pessoal do GitHub
-headers = {"Authorization": "Bearer xxxx"}
-
-# Consulta GraphQL
-query = """
-{
-  search(query: "stars:>1", type: REPOSITORY, first: 100) {
-    edges {
-      node {
-        ... on Repository {
-          name
-          createdAt
-          updatedAt
-          owner {
-            login
-          }
-          primaryLanguage {
-            name
-          }
-          pullRequests(states: MERGED) {
-            totalCount
-          }
-          releases {
-            totalCount
-          }
-          issues(states: CLOSED) {
-            totalCount
-          }
-          totalIssues: issues {
-            totalCount
-          }
-          stargazers {
-            totalCount
-          }
-        }
-      }
-    }
-  }
+headers = {
+    "Authorization": "Bearer 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",  # Substitua pelo seu token real
+    "Accept": "application/vnd.github.v3+json"
 }
-"""
 
-# Enviar a consulta
-response = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+def generate_query(after_cursor=None):
+    after_cursor_str = f', after: "{after_cursor}"' if after_cursor else ""
+    return f"""
+    {{
+      search(query: "stars:>1", type: REPOSITORY, first: 10{after_cursor_str}) {{
+        pageInfo {{
+          endCursor
+          hasNextPage
+        }}
+        edges {{
+          node {{
+            ... on Repository {{
+              name
+              createdAt
+              updatedAt
+              owner {{
+                login
+              }}
+              primaryLanguage {{
+                name
+              }}
+              pullRequests(states: MERGED) {{
+                totalCount
+              }}
+              releases {{
+                totalCount
+              }}
+              issues(states: CLOSED) {{
+                totalCount
+              }}
+              totalIssues: issues {{
+                totalCount
+              }}
+              stargazers {{
+                totalCount
+              }}
+            }}
+          }}
+        }}
+      }}
+    }}
+    """
 
-# Verificar se a consulta foi bem-sucedida
-if response.status_code == 200:
-    # Salvar a resposta em um arquivo JSON
-    with open('resultado_consulta.json', 'w') as file:
-        json.dump(response.json(), file, indent=4)
-    print("A resposta foi salva em 'resultado_consulta.json'")
-else:
-    print(f"Falha na consulta: {response.status_code}")
-    # Imprime a resposta para ajudar no diagnóstico do problema
-    print(response.text)
+def fetch_repositories():
+    has_next_page = True
+    after_cursor = None
+    all_repositories = []  # Lista para armazenar os dados dos repositórios
+    total_repos_collected = 0  # Contador para o número total de repositórios coletados
+
+    while has_next_page and total_repos_collected < 100:
+        query = generate_query(after_cursor)
+        response = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+        
+        if response.status_code == 200:
+            response_data = response.json()
+            edges = response_data['data']['search']['edges']
+            all_repositories.extend(edges)  # Adiciona os dados à lista
+            total_repos_collected += len(edges)  # Atualiza o contador de repositórios
+
+            # Preparar para a próxima página
+            page_info = response_data['data']['search']['pageInfo']
+            has_next_page = page_info['hasNextPage'] and total_repos_collected < 100
+            after_cursor = page_info['endCursor']
+        else:
+            print(f"Falha na consulta: {response.status_code}")
+            return  # Finaliza a função em caso de erro
+
+    # Cortar a lista para ter exatamente 100 repositórios, caso tenha excedido
+    all_repositories = all_repositories[:100]
+
+    # Após coletar os dados, escrever em um arquivo JSON
+    with open('consulta100rep.json', 'w') as file:
+        json.dump(all_repositories, file, indent=4)
+    print("Os dados dos repositórios foram salvos em 'consulta100rep.json'")
+
+# Chamar a função para iniciar a busca paginada e salvar os resultados
+fetch_repositories()
