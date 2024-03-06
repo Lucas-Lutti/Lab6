95854a65900c794a66550f76b670d4f049d20441
Lucas Picinin <lucaspicinin@hotmail.com>
2024-03-06 17:12:56 -0300

    Entrega2

--- Scripts/consulta_100repositorios_github.py
+++ Scripts/consulta_100repositorios_github.py
@@ -1,16 +1,17 @@
 import requests
 import json
+import csv
+from datetime import datetime
 
 headers = {
-    "Authorization": "Bearer xxxx",  # Substitua pelo seu token real
-    "Accept": "application/vnd.github.v3+json"
+    "Authorization": "Bearer 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",  # Substitua SEU_TOKEN_AQUI pelo seu token real
 }
 
 def generate_query(after_cursor=None):
     after_cursor_str = f', after: "{after_cursor}"' if after_cursor else ""
     return f"""
     {{
-      search(query: "stars:>1", type: REPOSITORY, first: 10{after_cursor_str}) {{
+      search(query: "stars:>1", type: REPOSITORY, first: 100{after_cursor_str}) {{
         pageInfo {{
           endCursor
           hasNextPage
@@ -28,9 +29,6 @@ def generate_query(after_cursor=None):
                 name
               }}
               pullRequests(states: MERGED) {{
-                totalCount
-              }}
-              releases {{
                 totalCount
               }}
               issues(states: CLOSED) {{
@@ -52,34 +50,48 @@ def generate_query(after_cursor=None):
 def fetch_repositories():
     has_next_page = True
     after_cursor = None
-    all_repositories = []  # Lista para armazenar os dados dos repositórios
-    total_repos_collected = 0  # Contador para o número total de repositórios coletados
+    all_repositories = []
 
-    while has_next_page and total_repos_collected < 100:
+    while has_next_page:
         query = generate_query(after_cursor)
         response = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
         
         if response.status_code == 200:
             response_data = response.json()
             edges = response_data['data']['search']['edges']
-            all_repositories.extend(edges)  # Adiciona os dados à lista
-            total_repos_collected += len(edges)  # Atualiza o contador de repositórios
+            all_repositories.extend([{
+                'name': edge['node']['name'],
+                'owner': edge['node']['owner']['login'],
+                'stars': edge['node']['stargazers']['totalCount'],
+                'createdAt': edge['node']['createdAt'],
+                'updatedAt': edge['node']['updatedAt'],
+                'primaryLanguage': edge['node']['primaryLanguage']['name'] if edge['node']['primaryLanguage'] else '',
+                'mergedPRs': edge['node']['pullRequests']['totalCount'],
+                
+                'closedIssues': edge['node']['issues']['totalCount'],
+                'totalIssues': edge['node']['totalIssues']['totalCount']
+            } for edge in edges])
 
-            # Preparar para a próxima página
             page_info = response_data['data']['search']['pageInfo']
-            has_next_page = page_info['hasNextPage'] and total_repos_collected < 100
+            has_next_page = page_info['hasNextPage']
             after_cursor = page_info['endCursor']
+
+            if len(all_repositories) >= 1000:
+                break
         else:
             print(f"Falha na consulta: {response.status_code}")
-            return  # Finaliza a função em caso de erro
+            break
 
-    # Cortar a lista para ter exatamente 100 repositórios, caso tenha excedido
-    all_repositories = all_repositories[:100]
+    return all_repositories[:1000]
 
-    # Após coletar os dados, escrever em um arquivo JSON
-    with open('consulta100rep.json', 'w') as file:
-        json.dump(all_repositories, file, indent=4)
-    print("Os dados dos repositórios foram salvos em 'consulta100rep.json'")
+def save_to_csv(repositories):
+    fields = ['name', 'owner', 'stars', 'createdAt', 'updatedAt', 'primaryLanguage', 'mergedPRs', 'releases', 'closedIssues', 'totalIssues']
+    with open('/Users/lucaspicinin/Desktop/Lab6/Laboratorio6.1/Lab6/Scripts/repositories.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fields)
+        writer.writeheader()
+        for repo in repositories:
+            writer.writerow(repo)
 
-# Chamar a função para iniciar a busca paginada e salvar os resultados
-fetch_repositories()
+repositories = fetch_repositories()
+save_to_csv(repositories)
+print("Dados dos repositórios foram salvos com sucesso em repositories.csv.")
